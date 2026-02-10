30e3e52f5bf0cc27ad60444a10715d8aa9231fdf
gutierrezvargasandy1 <gutierrezvargasandy1@gmail.com>
2026-02-09 19:56:51 -0600

    Crud copleto de Tareas

--- src/module/tarea/tarea.controller.ts
+++ src/module/tarea/tarea.controller.ts
@@ -1,13 +1,36 @@
-import { Controller, Get } from "@nestjs/common";
+import { Controller, Get, Post, Put, Delete, Param, Body } from "@nestjs/common";
 import { TareaService } from "./tarea.service";
 
 @Controller('api/tareas')
-export class TareaController{
-    constructor(private readonly tareaService : TareaService ){}
-    
-    @Get()
-    public mostrarTareaController() : String {
-           return this.tareaService.mostrarTareas();
+export class TareaController {
 
+    constructor(private readonly tareaService: TareaService) {}
+
+    @Post()
+    crearTarea(@Body() data: any) {
+        return this.tareaService.crearTarea(data);
+    }
+
+    @Get()
+    getTodas() {
+        return this.tareaService.getTodasLasTareas();
+    }
+
+    @Get(':id')
+    getById(@Param('id') id: string) {
+        return this.tareaService.getTareaById(Number(id));
+    }
+
+    @Put(':id')
+    actualizar(
+        @Param('id') id: string,
+        @Body() data: any
+    ) {
+        return this.tareaService.actualizarTarea(Number(id), data);
+    }
+
+    @Delete(':id')
+    eliminar(@Param('id') id: string) {
+        return this.tareaService.eliminarTarea(Number(id));
     }
 }
--- src/module/tarea/tarea.service.ts
+++ src/module/tarea/tarea.service.ts
@@ -1,10 +1,39 @@
 import { Injectable } from "@nestjs/common";
 
-@Injectable({})
+@Injectable()
 export class TareaService {
 
-    public mostrarTareas () : string {
-        return "Estas son las tareas de hoy"
+    public crearTarea(data: any) {
+        return {
+            mensaje: 'Tarea creada correctamente',
+            data
+        };
     }
 
+    public getTodasLasTareas() {
+        return {
+            mensaje: 'Lista de tareas obtenida',
+            tareas: []
+        };
+    }
+
+    public getTareaById(id: number) {
+        return {
+            mensaje: `Se obtuvo la tarea ${id}`,
+            tarea: { id }
+        };
+    }
+
+    public actualizarTarea(id: number, data: any) {
+        return {
+            mensaje: `Tarea ${id} actualizada correctamente`,
+            data
+        };
+    }
+
+    public eliminarTarea(id: number) {
+        return {
+            mensaje: `Tarea ${id} eliminada correctamente`
+        };
+    }
 }
