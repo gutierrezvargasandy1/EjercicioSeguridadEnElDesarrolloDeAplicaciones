0ef3c4e6fc3175598a46001a1e8924313cd29b1f
gutierrezvargasandy1 <gutierrezvargasandy1@gmail.com>
2026-04-08 17:42:26 -0600

    Todo listo version final con Validaciones

--- BackEnd/src/auth/dto/auth.dto.ts
+++ BackEnd/src/auth/dto/auth.dto.ts
@@ -2,11 +2,13 @@ import { IsNotEmpty, IsString, MaxLength, MinLength } from "class-validator";
 
 export class AuthDto {
 
-    @IsNotEmpty()
-    @IsString()
+    @IsNotEmpty({ message: 'El nombre de usuario es obligatorio'})
+    @IsString({ message: 'El nombre del usuario tiene que ser una cadena de texto'})
+    @MaxLength(50, { message: 'El nombre de usuario no puede tener más de 50 caracteres' })
     username: string;
     
-    @IsNotEmpty()
-    @IsString()
+    @IsNotEmpty({ message: 'La contraseña es obligatoria'})
+    @IsString({ message: 'La contraseña debe ser una cadena de texto'})
+    @MaxLength(100, { message: 'La contraseña no puede tener más de 100 caracteres' })
     password: string;
 }   
--- BackEnd/src/module/task/dto/create-task.dto.ts
+++ BackEnd/src/module/task/dto/create-task.dto.ts
@@ -2,16 +2,16 @@ import { IsBoolean, IsInt, IsNotEmpty, IsString, MaxLength, MinLength } from "cl
 
 export class CreateTaskDto {
 
-    @IsNotEmpty()
-    @IsString()
-    @MinLength(3) 
-    @MaxLength(50) 
+    @IsNotEmpty({ message: 'El nombre es Obligatorio'})
+    @MinLength(3 ,{ message: 'El nombre no tiene que tener menos de 3 caracteres'}) 
+    @IsString({ message: 'El nombre debe ser una cadena de texto' })
+    @MaxLength(50, { message: 'El nombre no tiene que tener mas de 50 caracteres'}) 
     name: string 
 
-    @IsNotEmpty()
-    @IsString()
-    @MinLength(3) 
-    @MaxLength(500) 
+    @IsNotEmpty({ message: 'La descripción es Obligatorio'})
+    @IsString({ message: 'La descripción debe ser una cadena de texto' })
+    @MinLength(3, { message: 'La descripción no tiene que tener menos de 3 caracteres'}) 
+    @MaxLength(500, { message: 'La descripción no tiene que tener mas de 500 caracteres'}) 
     description: string
 
     @IsNotEmpty()
