2f7c9e0845b4337648fc6b3785802998fe775e46
gutierrezvargasandy1 <gutierrezvargasandy1@gmail.com>
2026-04-08 18:13:56 -0600

    Todo listo version final con Validaciones

--- BackEnd/src/auth/auth.module.ts
+++ BackEnd/src/auth/auth.module.ts
@@ -5,6 +5,7 @@ import { JwtModule } from '@nestjs/jwt';
 import { UtilService } from 'src/common/services/util.service';
 import { PrismaService } from 'src/common/prisma/prisma.service';
 import { AuthGuard } from 'src/common/guards/auth.guard';
+import { UsersService } from 'src/module/users/users.service';
 
 @Module({
   imports:[
@@ -15,7 +16,7 @@ import { AuthGuard } from 'src/common/guards/auth.guard';
     }),
   ],
   controllers: [AuthController],
-  providers: [AuthService, UtilService, PrismaService, AuthGuard],
+  providers: [AuthService, UtilService, PrismaService, AuthGuard, UsersService],
   exports:[UtilService ,JwtModule , AuthService]
 })
 export class AuthModule {}
